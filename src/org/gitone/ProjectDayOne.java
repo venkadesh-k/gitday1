6c85ce94b05687f907854917aa21abba1670b54f
venkadesh-k <venkadeshjoy87@gmail.com>
2022-02-05 19:06:17 +0530

    demo project

---
+++ src/org/gitone/ProjectDayOne.java
@@ -0,0 +1,20 @@
+package org.gitone;
+
+public class ProjectDayOne {
+
+	public static void main(String[] args) {
+		
+	int a=12;
+	int b=21;
+	if (a>b) {
+		System.out.println("a is grater value");
+	}
+	else if (b>a) {
+		System.out.println("b is grater value");
+	}
+	else {
+		System.out.println("Both a & b valuse are equal");
+	}
+	
+	}
+}
